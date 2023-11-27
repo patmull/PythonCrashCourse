6a4d2fd17efc837822a0a807fabaabe115c236cb
patmull <patmull@seznam.cz>
2023-11-27 13:27:58 +0100

    Frequencies

--- AIBasics/die.py
+++ AIBasics/die.py
@@ -1,4 +1,6 @@
 from random import randint
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
 
 
 class Die:
@@ -8,3 +10,41 @@ class Die:
 
     def roll(self):
         return randint(1, self.num_sides)
+
+    def get_die_values(self):
+        return list(range(1, self.num_sides + 1))
+
+
+class GameOfDie:
+    def __init__(self, num_of_trials=1000):
+        self.results = []
+        self.die = Die()
+        self.num_of_trials = num_of_trials
+
+    def trials(self):
+
+        for i in range(self.num_of_trials):
+            self.results.append(self.die.roll())
+
+    def analyze_results(self, include_graph=False):
+
+        frequencies = []
+        if not len(self.results) > 0:
+            raise RuntimeError("Length of the results is 0. You need to perform the dice roll first.")
+        else:
+            # frequency analysis
+            for dice_num in self.die.get_die_values():
+                frequency = self.results.count(dice_num)
+                frequencies.append(frequency)
+
+        print(frequencies)
+
+        if include_graph == True:
+            x_values = self.die.get_die_values()
+            data = [Bar(x=x_values, y=frequencies)]
+
+            x_axis_config = {'title': 'Result'}
+            y_axis_config = {'title': "Frequency"}
+            layout = Layout(title=f"Die trials of {self.die.num_sides} die of {self.num_of_trials} trials.",
+                            xaxis=x_axis_config, yaxis=y_axis_config)
+            offline.plot({'data': data, 'layout': layout}, filename='die-trials-analyzed.html')
--- AIBasics/main.py
+++ AIBasics/main.py
@@ -1,4 +1,4 @@
-from die import Die
+from die import Die, GameOfDie
 from random_walk import RandomWalk
 
 if __name__ == '__main__':
@@ -8,9 +8,14 @@ if __name__ == '__main__':
 
     die = Die()
 
+    # Intro:
     die_attempts = []
     for roll_dice in range(100):
         roll = die.roll()
         die_attempts.append(roll)
-
     print(die_attempts)
+
+    # Analyzing the results
+    game_of_die = GameOfDie()
+    game_of_die.trials()
+    game_of_die.analyze_results(include_graph=True)
